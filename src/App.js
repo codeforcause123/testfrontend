4720f9e1facfdedd222c3dbca51228cd79e74d99
Aayush <aayush.130702@gmail.com>
2023-03-03 09:56:16 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch("https://testfullstackbackend-dgckekmog-aayush130702-gmailcom.vercel.app/api/data");
+      const json = await response.json();
+      if (response.ok) {
+        setData(json);
+      }
+    };
+    fetchdata();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="grid h-screen place-items-center">
+      <a
+        href="/api/data"
+        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          My Data
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {data.message}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </a>
+
     </div>
   );
 }
