9e4064fba89ff4e28880c28ec89d2ff5de0b6f12
Aayush <aayush.130702@gmail.com>
2023-03-11 09:03:19 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, React } from "react";
+import { Skeleton } from "@mui/material";
 
 function App() {
   const [data, setData] = useState([]);
@@ -17,17 +18,17 @@ function App() {
   }, []);
   return (
     <div className="grid h-screen place-items-center">
-      <a
-        href="/"
-        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      <div
+        class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700
+        w-40 h-32"
       >
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           My Data
         </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {data.message}
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+          {data.message || <Skeleton animation="wave"/>}
         </p>
-      </a>
+      </div>
     </div>
   );
 }
